ee5afdaa611c1ec8b0ea2c675cbee0dd26ce020a
JWangNov <wangjialun@126.com>
2020-04-11 20:32:20 -0400

    "Multi-Hello"

--- HelloProject/App.js
+++ HelloProject/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
-function HelloWorldApp() {
+const styles = StyleSheet.create({
+    center: {
+        alignItems: 'center'
+    }
+})
+
+function Greeting(props) {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-            <Text>Hello, world!</Text>
-            <Text>- JW -</Text>
+        <View style={styles.center}>
+            <Text>Hello {props.name}!</Text>
         </View>
-    )
+    );
 }
 
-export default HelloWorldApp;
+function LotsOfGreetings() {
+    return (
+        <View style={[styles.center, {top: 50}]}>
+            <Greeting name='0123'/>
+            <Greeting name='4567'/>
+            <Greeting name='890a'/>
+        </View>
+    );
+}
+
+export default LotsOfGreetings;
--- HelloProject/Readme.md
+++ HelloProject/Readme.md
@@ -11,3 +11,10 @@ Scan the QR code to deploy.
 ![Server Screenshot](screenshots/shw_0.png)
 
 ![Client Screenshot](screenshots/shw_1.jpeg)
+
+
+## Hello Props
+
+![Server Screenshot](screenshots/hp_0.png)
+
+![Client Screenshot](screenshots/hp_1.jpeg)
